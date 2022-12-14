a8d7ac0a4e1ef3f5f61e1ff924a88ae36b2eed17
Victor Rodrigo Freitas <victorrodrigofreitas@gmail.com>
2022-12-14 20:38:21 -0300

    Exercicio EX04 adicionado

---
+++ src/_08_introducao_poo/EX04/CurrencyConverter.java
@@ -0,0 +1,9 @@
+package _08_introducao_poo.EX04;
+
+public class CurrencyConverter {
+	public static double IOF = 0.06;
+
+	public static double dollarToReal(double amount, double dollarPrice) {
+		return amount * dollarPrice * (1.0 + IOF);
+	}
+}
